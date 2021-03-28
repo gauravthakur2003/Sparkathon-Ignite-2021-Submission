71e123c88f7fe4d6ec9e3dce57e24dfb46bc397d
Gaurav Thakur <69841670+gauravthakur2003@users.noreply.github.com>
2021-03-28 12:32:23 +0530

    Add files via upload

---
+++ loading.js
@@ -0,0 +1,40 @@
+document.addEventListener(
+    "DOMContentLoaded",
+    function() {
+      setTimeout(function() {    document.getElementById("loading").className = "slideDown";
+      }, 3800);
+    },
+    false
+  );
+  
+  document.addEventListener(
+    "DOMContentLoaded",
+    function() {
+      setTimeout(function() {
+        document.getElementById("loading-center").className = "zoomOut";
+      }, 3200);
+    },
+    false
+  );
+  
+  
+  document.addEventListener(
+    "DOMContentLoaded",
+    function() {
+      setTimeout(function() {
+        document.getElementById("block-1").className = "slideInUp";
+      }, 3800);
+    },
+    false
+  );
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
